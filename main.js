d42a6ce6dec84c08b38311be306c1cd0cbd305b4
claudeando <claude.mm6@gmail.com>
2022-11-08 19:31:51 +0000

    Tadao Ando

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { MeshStandardMaterial } from 'three'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
 
 const canvas = document.createElement('canvas')
 canvas.width = 650
@@ -12,7 +15,9 @@ window.addEventListener('load', init)
 function init() {
   // basic setup
   const scene = new THREE.Scene()
-  const camera = new THREE.OrthographicCamera(- 1.5, 1.5, 1.5, - 1.5, 0.0001, 1000)
+
+  const aspectRatio = canvas.width / canvas.height
+  const camera = new THREE.OrthographicCamera(- 1, 1, 1, - 1, 0.1, 100)
   camera.position.set(2, 2, 2)
   camera.lookAt(0, 0, 0)
 
@@ -21,13 +26,20 @@ function init() {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   renderer.setClearColor('white')
   renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   document.body.append(renderer.domElement)
 
+  const effectComposer = new EffectComposer(renderer)
+  effectComposer.setSize(canvas.width, canvas.height)
+  effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  const renderPass = new RenderPass(scene, camera)
+  effectComposer.addPass(renderPass)
+
   const controls = new OrbitControls(camera, renderer.domElement)
+  controls.autoRotate = true
   controls.enableDamping = true
   controls.dampingFactor = 0.125
   controls.update()
-
 
 
   const fog = new THREE.Fog('white', 2, 6)
@@ -35,23 +47,42 @@ function init() {
 
 
 
+  // light setup
+  const ambLight = new THREE.AmbientLight()
+  ambLight.intensity = 0.1
+
+  const dirLight = new THREE.DirectionalLight()
+  dirLight.shadow.mapSize.width = 1024
+  dirLight.shadow.mapSize.height = 1024
+  // dirLight.shadow.radius = 100
+  dirLight.position.set(6.5, 2.5, 5)
+  dirLight.castShadow = true
+  dirLight.lookAt(0, 0, 0)
+
+  scene.add(ambLight)
+
+
+
+
+
   // objects setup
   const whole = new THREE.Group()
 
-
+  // ground
   const groundGeo = new THREE.PlaneGeometry(15, 15)
   const groundMat = new THREE.MeshStandardMaterial()
   const ground = new THREE.Mesh(groundGeo, groundMat)
   ground.rotation.x = -Math.PI / 2
   ground.receiveShadow = true
 
-  const churchMat = new THREE.MeshLambertMaterial({ color: 'lightgrey' })
 
+  // church
+  const churchMat = new THREE.MeshLambertMaterial({ color: 'lightgrey' })
   const church = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 0.65, 0.5),
+    new THREE.BoxGeometry(1, 0.5, 0.5),
     churchMat
   )
-  church.position.y = .65 / 2
+  church.position.y = .5 / 2
   church.castShadow = true
 
   const churchBase = new THREE.Mesh(
@@ -65,32 +96,27 @@ function init() {
   // church cross
   const cross = new THREE.Group()
 
+  const crossMat = new THREE.MeshStandardMaterial({
+    emissive: 'white',
+    emissiveIntensity: 1.5
+  })
   const crossX = new THREE.Mesh(
-    new THREE.BoxGeometry(0.05, 1.15, 0.025),
-    new THREE.MeshStandardMaterial()
+    new THREE.BoxGeometry(0.035, 0.9, 0.025),
+    crossMat
   )
-  crossX.position.y = -0.1
+  crossX.position.y = - 0.25
+
   const crossY = new THREE.Mesh(
-    new THREE.BoxGeometry(0.75, 0.05, 0.025),
-    new THREE.MeshStandardMaterial()
+    new THREE.BoxGeometry(0.75, 0.035, 0.025),
+    crossMat
   )
+  crossY.position.y = - 0.15
 
   cross.add(crossX, crossY)
   cross.scale.set(0.5, 0.5)
   cross.position.x = 0.51
   cross.position.y = 0.35
   cross.rotation.y = Math.PI / 2
-
-
-  // grass 
-  const grassGeo = new THREE.SphereGeometry(0.1, 32, 32)
-  const grassMat = new THREE.MeshStandardMaterial({ color: 'darkgreen' })
-  for (let i = 0; i < 25; i++) {
-    const grass = new THREE.Mesh(grassGeo, grassMat)
-    grass.position.x = (Math.random() - .5) * 2.5
-    grass.position.z = (Math.random() - .5) * 2.5
-    // scene.add(grass)
-  }
 
 
   // chairs
@@ -114,19 +140,40 @@ function init() {
     bench.castShadow = true
     bench.receiveShadow = true
   }
+  for (let i = 0; i < 15; i++) {
+    const bench = new THREE.Mesh(benchGeo, benchMat)
+    bench.position.x = i / 8 - 1.015
+    bench.position.y = 0.05
+    bench.position.z = -.85
+    bench.rotation.z = Math.PI / 1.75
+    scene.add(bench)
+    bench.castShadow = true
+    bench.receiveShadow = true
+  }
+  for (let i = 0; i < 15; i++) {
+    const bench = new THREE.Mesh(benchGeo, benchMat)
+    bench.position.x = i / 8 - 1.015
+    bench.position.y = 0.05
+    bench.position.z = .85
+    bench.rotation.z = Math.PI / 1.75
+    scene.add(bench)
+    bench.castShadow = true
+    bench.receiveShadow = true
+  }
 
 
-  // light setup
-  const ambLight = new THREE.AmbientLight()
-  ambLight.intensity = 0.25
 
-  const dirLight = new THREE.DirectionalLight()
-  dirLight.position.set(6.5, 2.5, 5)
-  dirLight.castShadow = true
+  // grass 
+  const center = new THREE.Mesh(
+    new THREE.SphereGeometry(0.01, 16, 16),
+    new THREE.MeshStandardMaterial({ color: 'black' })
+  )
+  center.add(dirLight)
+  scene.add(center)
 
-  scene.add(ground, church, churchBase, cross, ambLight, dirLight)
 
 
+  scene.add(ground, church, churchBase, cross)
 
 
 
@@ -138,6 +185,8 @@ function init() {
     camera.updateProjectionMatrix()
     renderer.setSize(canvas.width, canvas.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    effectComposer.setSize(canvas.width, canvas.height)
+    effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   })
 
 
@@ -145,12 +194,15 @@ function init() {
   const clock = new THREE.Clock()
 
   const animate = () => {
-    const elapsedTime = clock.getElapsedTime
+    const elapsedTime = clock.getElapsedTime()
+
+    center.rotation.x = elapsedTime
 
     controls.update()
 
     renderer.render(scene, camera)
     requestAnimationFrame(animate)
+    effectComposer.render()
   }
   animate()
 }
